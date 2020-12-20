a34925b1ad33ee16e43f62c320d73215e4ce2615
Vincent <35348750+VinceKaj@users.noreply.github.com>
2020-12-21 00:43:43 +0200

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const mongoose = require('mongoose');
+const mongoPath = process.env.MONGO_URL;
+
+module.exports = async () => {
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    });
+    return mongoose;
+}
